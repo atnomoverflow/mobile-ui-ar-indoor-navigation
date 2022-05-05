9c73e663641dddcb9cf5b119ed7ac2d4ee569117
atnomoverflow <montaser-abdmajid.zehri@enis.tn>
2022-05-05 10:59:59 +0100

    done with the ducks pattern set up

---
+++ state/ducks/marker.ts
@@ -0,0 +1,54 @@
+import { Action, Marker } from "../../types";
+import { AnyAction, Dispatch } from 'redux';
+
+const MARKER_REGISTER = 'ArNavigationForMuseum/marker/REGISTER';
+const MARKER_RESET = 'ArNavigationForMuseum/marker/RESET';
+
+const reset = (): Action => ({
+    type: MARKER_RESET
+})
+
+
+const register = (marker: Marker): Action => ({
+    type: MARKER_REGISTER,
+    payload: marker
+});
+
+const fetchMarker = (id: string) => {
+    return (function (dispatch: Dispatch<AnyAction>) {
+        fetch(`localhost:3000/marker/${id}`)
+            .then((response) => response.json())
+            .then((data) => {
+                dispatch(register(data));
+            });
+    });
+};
+const actionTypes = {
+    MARKER_REGISTER,
+    MARKER_RESET
+};
+
+const initialState = {}
+const actionCreators = {
+    fetchMarker,
+    reset
+};
+const reducer = function (state = initialState, action: Action) {
+    switch (action.type) {
+        case MARKER_REGISTER: {
+            const entry = action.payload;
+            const { id, longitude, laltitude } = entry;
+
+            return { ...state, id, longitude, laltitude };
+        }
+        case MARKER_RESET:{
+            return initialState
+        }
+        default:
+            return state;
+    }
+};
+
+export { actionCreators, actionTypes, initialState };
+
+export default reducer;
--- types.tsx
+++ types.tsx
@@ -6,6 +6,7 @@
 import { BottomTabScreenProps } from '@react-navigation/bottom-tabs';
 import { CompositeScreenProps, NavigatorScreenParams } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import {store} from './state';
 
 declare global {
   namespace ReactNavigation {
@@ -15,7 +16,8 @@ declare global {
 
 export type RootStackParamList = {
   QrScanner: undefined;
-  ArSceeneScreen:  undefined;  
+  ArSceeneScreen:  undefined;
+  ListWayPoint:undefined;  
 };
 
 export type RootStackScreenProps<Screen extends keyof RootStackParamList> = NativeStackScreenProps<
@@ -32,3 +34,22 @@ export type RootTabScreenProps<Screen extends keyof RootTabParamList> = Composit
   BottomTabScreenProps<RootTabParamList, Screen>,
   NativeStackScreenProps<RootStackParamList>
 >;
+export type WayPoint={
+  longitude:number;
+  laltitude:number;
+  name:string;
+  image:string;
+id:string
+}
+export type Marker={
+  longitude:number;
+  laltitude:number;
+id:string
+}
+export type Action={
+  type:string,
+  payload?:any
+}
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
